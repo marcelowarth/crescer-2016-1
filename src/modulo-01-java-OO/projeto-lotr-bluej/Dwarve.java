a2c6648d188d5879e601a0d43f5d0968c83dc676
Marcelo Moura <Marcelo Moura>
2016-04-06 09:49:43 -0300

    Refactor(elfo): atirar flecha em dwarve recebe qual dwarve deve receber dano Add(dwarve): classe dwarve com quantidade de vida

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Dwarve.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Dwarve here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Dwarve
+{
+    // instance variables - replace the example below with your own
+    private int vida = 110;
+
+    /**
+     * Constructor for objects of class Dwarve
+     */
+    public Dwarve(String nome)
+    {
+    }
+
+    public void recebeDano()
+    {
+        vida = vida - 10;
+    }
+}
--- src/modulo-01-java-OO/projeto-lotr-bluej/Elfo.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Elfo.java
@@ -1,8 +1,6 @@
 public class Elfo {
     private String nome;
     private int experiencia, flechas = 42;
-    private int vidaDwarve = 110;
-    private boolean dwarveVivo = true;
     
     public Elfo(String nome) {
         this.nome = nome;
@@ -13,23 +11,9 @@ public class Elfo {
         flechas--;
     }
     
-    public void atirarFlechaDwarves() {
-        if(dwarveVivo == true){
-            if(vidaDwarve > 0) {
-                experiencia++;
-                flechas--;
-                vidaDwarve = vidaDwarve - 10;
-            } else
-                dwarveVivo = false;
-        } else {
-            reviveDwarve();
-        }
-            
-            
-    }
-    
-    public void reviveDwarve(){
-        dwarveVivo = true;
-        vidaDwarve = 110;
+    public void atirarFlechaDwarves(Dwarve dwarve) {
+        dwarve.recebeDano();
+        experiencia++;
+        flechas--;
     }
 }
