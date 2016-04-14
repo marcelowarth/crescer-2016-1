1894d3169e0f48c1f3b07a1dcdb3ad9012976002
Marcelo Warth de Moura <marcelo.moura@cwinet.local>
2016-04-14 17:06:32 -0300

    feat(DicionarioSindarin): criada interface de tradução do sindarin para qualquer palavra

--- src/modulo-01-java-OO/projeto-lotr-bluej/DicionarioSindarin.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/DicionarioSindarin.java
@@ -3,6 +3,27 @@ import java.util.*;
 public class DicionarioSindarin
 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bem vindo ao tradutor Sindarin! Escolha a linguagem: ");
+        System.out.println("[P]ortuguês [I]nglês");
+        String idioma = scanner.nextLine();
+        TradutorSindarin tradutor = null;
+        switch (idioma.toLowerCase()) {
+            case "i":
+                tradutor = new SindarinParaIngles();
+                break;
+            default:
+                tradutor = new SindarinParaPortugues();
+                break;
+        }
+        System.out.println("Digite a palavra a traduzir: ");
+        String palavra = scanner.nextLine();
+        System.out.println("Palavra " + palavra + " significa " + 
+                        tradutor.traduzir(palavra.toLowerCase()));
+        // radarExemplosHashMap();
+    }
+    
+    private static void rodarExemplosHashMap() {
         HashMap<String, String> dicionarioSindarin = new HashMap<>();
         dicionarioSindarin.put("terra", "amar");
         dicionarioSindarin.put("fogo", "naur");
