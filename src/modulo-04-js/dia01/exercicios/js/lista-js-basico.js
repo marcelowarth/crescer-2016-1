0a67d7e95d5ac419d76ee738747eb8c96a98806c
Marcelo Moura <Marcelo Moura>
2016-05-02 21:57:25 -0300

    feat(exercicios dia 01): resolucao dos exercicios, menos o 7

--- src/modulo-04-js/dia01/exercicios/js/lista-js-basico.js
+++ src/modulo-04-js/dia01/exercicios/js/lista-js-basico.js
@@ -19,14 +19,165 @@ Faça uma função chamada maiorTexto que receba um array de strings e retorne o
 
 function maiorTexto(array) {
   var maior = undefined;
-  for (var i in array) {
+  for (var i = 0; i < array.length; i++) {
     if(typeof maior === 'undefined') {
-      maior = i;
+      maior = array[i];
     } else {
-      if(maior.lenght < i.lenght) {
-        maior = i;
+      if(maior.length < array[i].length) {
+        maior = array[i];
       }
     }
   }
   console.log(maior);
 }
+
+/*
+Instrutor querido
+
+Faça uma função chamada imprime que receba dois parâmetros:
+
+Um array de strings; e
+Uma função.
+Dentro da função imprime chame a função do segundo parâmetro para cada elemento do array. Exemplo:
+
+  imprime(
+    // primeiro parâmetro: array
+    [ 'bernardo', 'nunes', 'fabrício', 'ben-hur', 'carlos' ],
+    // segundo parâmetro: função
+    function(instrutor) {
+     console.log('olá querido instrutor:', instrutor)
+    }
+  );
+Deve resultar em:
+
+"olá querido instrutor: bernardo"
+"olá querido instrutor: nunes"
+"olá querido instrutor: fabrício"
+"olá querido instrutor: ben-hur"
+"olá querido instrutor: carlos"
+
+Atenção! Faça um tratamento para evitar que a função imprime seja chamada com um segundo parâmetro que não seja uma função, por exemplo:
+
+imprime([ 'bernardo', 'nunes', 'fabrício', 'ben-hur', 'carlos' ], 3.14);
+// Jabulani:
+// 'TypeError: number is not a function'
+*/
+function instrutor(value) {
+   console.log('olá querido instrutor:', value);
+}
+
+function imprime(array, instrutor) {
+  if(typeof instrutor === 'funtion') {
+    for (var i = 0; i < array.length; i++) {
+      instrutor(array[i]);
+    }
+  } else {
+    throw new TypeError('second parameter is not a valid function, should be "instrutor"');
+  }
+}
+/*
+Soma diferentona
+
+Escreva uma função somar que permite somar dois números através de duas chamadas diferentes (não necessariamente pra mesma função). Pirou? Ex:
+
+adicionar(3)(4); // 7
+adicionar(5642)(8749); // 14391
+*/
+
+function adicionar(x){
+  return function(y){
+    console.log(x + y);
+  };
+};
+
+/*
+Fibona
+
+Faça uma função fiboSum que calcule a soma da sequência de Fibonacci para n números informados. Exemplo de chamada:
+
+fiboSum(7);
+// 33 (soma dos 7 primeiros números da sequência: 1+1+2+3+5+8+13)
+Dica: aproveite toda "beleza" dos algoritmos recursivos! #sqn
+*/
+function fiboSum(indice) {
+  var i;
+  var fib = []; // Initialize array!
+
+  fib[0] = 0;
+  fib[1] = 1;
+  soma = 1;
+  for(i=2; i<=indice; i++)
+  {
+      // Next fibonacci number = previous + one before previous
+      // Translated to JavaScript:
+      fib[i] = fib[i-2] + fib[i-1];
+      soma = soma + fib[i];
+  }
+  console.log(soma);
+}
+/*
+Quero café
+
+Escreva uma função queroCafe que recebe dois parâmetros:
+
+mascada: Valor numérico
+precos: Lista de preços de café
+A sua implementação deve retornar uma string com todos os preços que estão abaixo ou igual ao valor mascada ordenados de forma ascendente e separados por ,. Ex:
+
+queroCafe(3.14, [ 5.16, 2.12, 1.15, 3.11, 17.5 ]);
+// '1.15,2.12,3.11'
+*/
+
+function queroCafe(mascada, array) {
+  var ordena = array.sort(function(a, b){return a-b});
+  for (var i = 0; i < ordena.length; i++) {
+    if (ordena[i] > mascada) {
+      ordena.splice(i,1);
+      i--;
+    }
+  }
+  console.log(ordena);
+}
+/*
+Vários tipos
+
+Implemente uma função contarPorTipo que recebe dois argumentos:
+
+Objeto que pode ter várias propriedades, de vários tipos diferentes
+Uma string com os seguintes possíveis valores:
+'string'
+'number'
+'boolean'
+'object'
+'undefined'
+'null'
+'function'
+'array'
+A função deve contar quantas propriedades dentro do objeto informado no primeiro parâmetro são do tipo do segundo parâmetro.
+*/
+
+function contarPorTipo (obj, tipo) {
+  var eValido = false;
+  var validos = ['string', 'number', 'boolean', 'object', 'undefined', 'null', 'function', 'array'];
+
+  if(validos.indexOf(typeof tipo)) {
+    eValido = true;
+  }
+}
+
+/*
+Reflexões
+
+Analise o código abaixo e coloque um comentário no seu arquivo JS explicando o resultado.
+*/
+var gohan = 'gohan', goku = 'Goku';
+function revelarSaiyaman() {
+  console.log(gohan);
+  goku = 'Son Goku';
+  var gohan = 'Son ' + gohan;
+  return gohan;
+}
+console.log(revelarSaiyaman());
+console.log(goku);
+
+// O espaco de memoria da variavel e setado anteriormente aos valores da mesma, os valores so são setados quando a execucao chega na linha de atribuicao, quando a function e chamada, a variavel é redeclarada antes mesmo de receber um valor;
