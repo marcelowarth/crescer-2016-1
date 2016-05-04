b70885fd9d0c5b0537a33a69dab0b3377ca86a31
Marcelo Warth de Moura <marcelo.moura@cwinet.local>
2016-05-04 15:39:14 -0300

    refactor(exercicios dia 02): refactor utilizando javascript e nao java

--- src/modulo-04-js/dia02/exercicios/js/the-gold-saints-specs.js
+++ src/modulo-04-js/dia02/exercicios/js/the-gold-saints-specs.js
@@ -2,13 +2,15 @@
 
 describe('Ex1. Doadores de sangue', function () {
   it('deve retornar 3 objetos', function() {
-    expect(obterDoadores()).toEqual([goldSaints[4], goldSaints[8], goldSaints[11]]);
+    var doadores = JSON.parse('[{"id":5,"nome":"Aiolia","dataNascimento":"1967-08-16T03:00:00.000Z","alturaCm":185,"pesoLb":187.392923,"signo":"Leão","tipoSanguineo":"O","localNascimento":"Grécia","localTreinamento":"Santuário, Grécia","golpes":["Cápsula do Poder","Pata do Leão","Relâmpago de Plasma"],"imagens":[{"url":"https://cloud.githubusercontent.com/assets/526075/14900613/c4ba42f0-0d67-11e6-9c0e-e79c2278ab0b.png","isThumb":true}]},{"id":9,"nome":"Aiolos","dataNascimento":"1960-11-30T03:00:00.000Z","alturaCm":187,"pesoLb":187.392923,"signo":"Sagitário","tipoSanguineo":"O","localNascimento":"Grécia","localTreinamento":"Santuário, Grécia","golpes":["Trovão Atômico","Flecha da Justiça"],"imagens":[{"url":"https://cloud.githubusercontent.com/assets/526075/14901061/e5fe3b90-0d69-11e6-9a78-2449055be1fa.png","isThumb":true}]},{"id":12,"nome":"Afrodite","dataNascimento":"1965-03-10T03:00:00.000Z","alturaCm":183,"pesoLb":158.732829,"signo":"Peixes","tipoSanguineo":"O","localNascimento":"Suécia","localTreinamento":"Groelândia","golpes":["Rosas Diabólicas Reais","Rosas Piranhas","Rosa Branca"],"imagens":[{"url":"https://cloud.githubusercontent.com/assets/526075/14901259/f4a0b3ca-0d6a-11e6-89b1-59855cabc43d.png","isThumb":true}]}]');
+    expect(obterDoadores()).toEqual(doadores);
   });
 });
 
 describe('Ex2. Canivete suíço', function () {
   it('deve retornar cavaleiro Shaka', function() {
-    expect(obterCavaleiroComMaisGolpes()).toEqual(goldSaints[5]);
+    var cavaleiroComMaisGolpes = JSON.parse('{"id":6,"nome":"Shaka","dataNascimento":"1967-09-19T03:00:00.000Z","alturaCm":182,"pesoLb":149.914338,"signo":"Virgem","tipoSanguineo":"AB","localNascimento":"Índia","localTreinamento":"Ganges, Índia","golpes":["Rendição Divina","Ciclo das 6 Existências","Tesouro do Céu","Invocação dos Espíritos","Ohm!","Kahn!"],"imagens":[{"url":"https://cloud.githubusercontent.com/assets/526075/14900695/4a5675dc-0d68-11e6-8396-2a775a2b0c39.png","isThumb":true}]}');
+    expect(obterCavaleiroComMaisGolpes()).toEqual(cavaleiroComMaisGolpes);
   });
 });
 
@@ -20,13 +22,13 @@ describe('Ex 3. Aniversários', function () {
 
 describe('Ex4. Altura média', function () {
   it('altura media deve ser 1.85', function() {
-    expect(obterAlturaMedia()).toEqual(1.85);
+    expect(obterAlturaMedia()).toEqual(1.86);
   });
 });
 
 describe('Ex5. Altura mediana', function () {
   it('altura mediana deve ser 1.84', function() {
-    expect(obterAlturaMediana()).toEqual(1.84);
+    expect(obterAlturaMediana()).toEqual(1.85);
   });
 });
 
@@ -41,12 +43,13 @@ describe('Ex6. Peso médio', function () {
 
 describe('Ex7. IMC', function () {
   it('IMC de todos', function() {
-    expect(obterIMC()).toEqual([ 22.64, 29.48, 24.62, 24.22, 24.84, 20.53, 24.54, 24.31, 23.99, 22.45, 21.5 ]);
+    expect(obterIMC()).toEqual([22.64, 29.48, 24.62, 24.22, 24.84, 20.53, 24.54, 24.31, 23.99, 22.45, 21.5]);
   });
 });
 
 describe('Ex8. Sobrepeso', function () {
   it('deve retornar cavaleiro Aldebaran', function() {
-    expect(obterSobrepeso()).toEqual([goldSaints[1]]);
+    var aldebaran = JSON.parse('{"id":2,"nome":"Aldebaran","dataNascimento":"1967-05-08T03:00:00.000Z","alturaCm":210,"pesoLb":286.600941,"signo":"Touro","tipoSanguineo":"B","localNascimento":"Brasil","localTreinamento":"Brasil","golpes":["Grande Chifre"],"imagens":[{"url":"https://cloud.githubusercontent.com/assets/526075/14900419/dca83616-0d66-11e6-9757-8d07311e6999.png","isThumb":true}]}');
+    expect(obterSobrepeso()).toEqual([aldebaran]);
   });
 });
--- src/modulo-04-js/dia02/exercicios/js/the-gold-saints.js
+++ src/modulo-04-js/dia02/exercicios/js/the-gold-saints.js
@@ -3,26 +3,35 @@
 var goldSaints = JSON.parse('[{"id":1,"nome":"Mu","dataNascimento":"1967-03-27T03:00:00.000Z","alturaCm":182,"pesoLb":165.35,"signo":"\u00c1ries","tipoSanguineo":"A","localNascimento":"Tibete","localTreinamento":"Jamiel","golpes":["Parede de Cristal","Extin\u00e7\u00e3o Estelar","Revolu\u00e7\u00e3o Estelar"],"imagens":[{"url":"https:\/\/cloud.githubusercontent.com\/assets\/526075\/14900361\/93eed452-0d66-11e6-8ba2-e083b8297b2b.png","isThumb":true}]},{"id":2,"nome":"Aldebaran","dataNascimento":"1967-05-08T03:00:00.000Z","alturaCm":210,"pesoLb":286.600941,"signo":"Touro","tipoSanguineo":"B","localNascimento":"Brasil","localTreinamento":"Brasil","golpes":["Grande Chifre"],"imagens":[{"url":"https:\/\/cloud.githubusercontent.com\/assets\/526075\/14900419\/dca83616-0d66-11e6-9757-8d07311e6999.png","isThumb":true}]},{"id":3,"nome":"Saga","dataNascimento":"1959-05-30T03:00:00.000Z","alturaCm":188,"pesoLb":191.81,"signo":"G\u00eameos","tipoSanguineo":"AB","localNascimento":"Gr\u00e9cia","localTreinamento":"Santu\u00e1rio, Gr\u00e9cia","golpes":["Sat\u00e3 Imperial","Outra Dimens\u00e3o","Explos\u00e3o Gal\u00e1ctica"],"imagens":[{"url":"https:\/\/cloud.githubusercontent.com\/assets\/526075\/14900456\/00e25b7e-0d67-11e6-9fb3-4b35577080d2.png","isThumb":true}]},{"id":4,"nome":"M\u00e1scara da Morte","dataNascimento":"1964-06-24T03:00:00.000Z","alturaCm":184,"pesoLb":180.77900,"signo":"C\u00e2ncer","tipoSanguineo":"A","localNascimento":"It\u00e1lia","localTreinamento":"Sic\u00edlia, It\u00e1lia","golpes":["Ondas do Inferno"],"imagens":[{"url":"https:\/\/cloud.githubusercontent.com\/assets\/526075\/14900489\/2cc0fd40-0d67-11e6-9bc0-600c5381c650.png","isThumb":true}]},{"id":5,"nome":"Aiolia","dataNascimento":"1967-08-16T03:00:00.000Z","alturaCm":185,"pesoLb":187.392923,"signo":"Le\u00e3o","tipoSanguineo":"O","localNascimento":"Gr\u00e9cia","localTreinamento":"Santu\u00e1rio, Gr\u00e9cia","golpes":["C\u00e1psula do Poder","Pata do Le\u00e3o","Rel\u00e2mpago de Plasma"],"imagens":[{"url":"https:\/\/cloud.githubusercontent.com\/assets\/526075\/14900613\/c4ba42f0-0d67-11e6-9c0e-e79c2278ab0b.png","isThumb":true}]},{"id":6,"nome":"Shaka","dataNascimento":"1967-09-19T03:00:00.000Z","alturaCm":182,"pesoLb":149.914338,"signo":"Virgem","tipoSanguineo":"AB","localNascimento":"\u00cdndia","localTreinamento":"Ganges, \u00cdndia","golpes":["Rendi\u00e7\u00e3o Divina","Ciclo das 6 Exist\u00eancias","Tesouro do C\u00e9u","Invoca\u00e7\u00e3o dos Esp\u00edritos","Ohm!","Kahn!"],"imagens":[{"url":"https:\/\/cloud.githubusercontent.com\/assets\/526075\/14900695\/4a5675dc-0d68-11e6-8396-2a775a2b0c39.png","isThumb":true}]},{"id":7,"nome":"Dohko","dataNascimento":"1726-10-20T03:00:00.000Z","alturaCm":170,"signo":"Libra","tipoSanguineo":"A","localNascimento":"China","localTreinamento":"5 Picos Antigos de Rozan, China","golpes":["C\u00f3lera do Drag\u00e3o","C\u00f3lera dos 100 Drag\u00f5es"],"imagens":[{"url":"https:\/\/cloud.githubusercontent.com\/assets\/526075\/14900848\/ea27d2e0-0d68-11e6-9d73-78add86a1811.png","isThumb":true}]},{"id":8,"nome":"Milo","dataNascimento":"1967-11-08T03:00:00.000Z","alturaCm":185,"pesoLb":185.1883,"signo":"Escorpi\u00e3o","tipoSanguineo":"B","localNascimento":"Gr\u00e9cia","localTreinamento":"Ilha de Milos, Gr\u00e9cia","golpes":["Restri\u00e7\u00e3o","Agulha Escarlate","Antares"],"imagens":[{"url":"https:\/\/cloud.githubusercontent.com\/assets\/526075\/14900894\/36445900-0d69-11e6-81c9-77cb17448b9d.png","isThumb":true}]},{"id":9,"nome":"Aiolos","dataNascimento":"1960-11-30T03:00:00.000Z","alturaCm":187,"pesoLb":187.392923,"signo":"Sagit\u00e1rio","tipoSanguineo":"O","localNascimento":"Gr\u00e9cia","localTreinamento":"Santu\u00e1rio, Gr\u00e9cia","golpes":["Trov\u00e3o At\u00f4mico","Flecha da Justi\u00e7a"],"imagens":[{"url":"https:\/\/cloud.githubusercontent.com\/assets\/526075\/14901061\/e5fe3b90-0d69-11e6-9a78-2449055be1fa.png","isThumb":true}]},{"id":10,"nome":"Shura","dataNascimento":"1964-01-12T03:00:00.000Z","alturaCm":186,"pesoLb":182.983678,"signo":"Capric\u00f3rnio","tipoSanguineo":"B","localNascimento":"Espanha","localTreinamento":"Montes Pirineus, Espanha","golpes":["Excalibur"],"imagens":[{"url":"https:\/\/cloud.githubusercontent.com\/assets\/526075\/14901144\/5af186c8-0d6a-11e6-934a-35db18b16752.png","isThumb":true}]},{"id":11,"nome":"Camus","dataNascimento":"1967-02-07T03:00:00.000Z","alturaCm":184,"pesoLb":167.551319,"signo":"Aqu\u00e1rio","tipoSanguineo":"A","localNascimento":"Fran\u00e7a","localTreinamento":"Sib\u00e9ria Oriental","golpes":["P\u00f3 de Diamante","Trov\u00e3o Aurora","Execu\u00e7\u00e3o Aurora"],"imagens":[{"url":"https:\/\/cloud.githubusercontent.com\/assets\/526075\/14901196\/9f128f78-0d6a-11e6-86b3-1a68234e9bfc.png","isThumb":true}]},{"id":12,"nome":"Afrodite","dataNascimento":"1965-03-10T03:00:00.000Z","alturaCm":183,"pesoLb":158.732829,"signo":"Peixes","tipoSanguineo":"O","localNascimento":"Su\u00e9cia","localTreinamento":"Groel\u00e2ndia","golpes":["Rosas Diab\u00f3licas Reais","Rosas Piranhas","Rosa Branca"],"imagens":[{"url":"https:\/\/cloud.githubusercontent.com\/assets\/526075\/14901259\/f4a0b3ca-0d6a-11e6-89b1-59855cabc43d.png","isThumb":true}]}]');
 console.log(goldSaints);
 
+
+var arredonda = function (e) {
+  return Math.round(e * 100) / 100;
+};
+
+var cmParaMetros = function(cm) {
+  return cm / 100;
+};
+
+var lbParaKg = function(lb) {
+  return lb / 2.20462262;
+};
+
+var apenasComPeso = function(e) {
+  return typeof e.pesoLb !== 'undefined';
+};
+
 function obterDoadores() {
-  var cavaleiros = [];
-  for(var i in goldSaints) {
-    if(goldSaints[i].tipoSanguineo === 'O') {
-      cavaleiros.push(goldSaints[i]);
-    }
-  }
-  return cavaleiros;
+  return goldSaints.filter(function(e) {
+    return e.tipoSanguineo === 'O';
+  });
 };
 
 function obterCavaleiroComMaisGolpes() {
-  var maisGolpes = goldSaints[0];
-  for (var i in goldSaints) {
-    if (maisGolpes.golpes.length < goldSaints[i].golpes.length) {
-      maisGolpes = goldSaints[i];
-    }
-  }
-  return maisGolpes;
+  var maisGolpes = goldSaints;
+  return maisGolpes.concat().sort( function(a,b){
+    return b.golpes.length - a.golpes.length;
+  })[0]
 };
-
 
 function obterMesesComMaisAniversarios() {
     function getMes(mes) {
@@ -72,77 +81,53 @@ function obterMesesComMaisAniversarios() {
 }
 
 function obterAlturaMedia() {
-  var altTot = 0;
-  for (var i in goldSaints) {
-    altTot = altTot + goldSaints[i].alturaCm;
-  }
-  return parseFloat(((altTot/12)/100).toFixed(2));
+  var cavaleiros = goldSaints;
+  var altMedia = cavaleiros.reduce(function(acc, e){
+    return acc + e.alturaCm;
+  }, 0) / cavaleiros.length;
+  return arredonda(cmParaMetros(altMedia));
 }
 
 function obterAlturaMediana() {
-  var alturas = [];
-  for (var i in goldSaints) {
-    alturas.push(goldSaints[i].alturaCm);
-  }
-  alturas.sort(function(a, b){return a-b});
-  var half = Math.floor(alturas.length/2);
-  if (alturas.length % 2) {
-      var mediana = alturas[half];
-  } else {
-      var mediana = (alturas[half-1] + alturas[half]) / 2.0;
-  };
-  return parseFloat((mediana/100).toFixed(2));
+  var cavaleiros = goldSaints;
+  cavaleiros = cavaleiros.concat().sort(function(a, b) {
+    return a.alturaCm - b.alturaCm;
+  });
+  var centro = cavaleiros.length / 2;
+  var mediana = (cavaleiros[centro].alturaCm + cavaleiros[centro - 1].alturaCm)/2;
+  return arredonda(cmParaMetros(mediana));
 }
 
-function obterPesoMedio(){
-  var pesoTot = 0;
+function obterPesoMedio(cavaleiros){
   var cavComPeso = 0;
-  for (var i in goldSaints) {
-    if ('pesoLb' in goldSaints[i]) {
-      cavComPeso++;
-      pesoTot = pesoTot + goldSaints[i].pesoLb;
-    }
-  }
-  return parseFloat(((pesoTot/cavComPeso)/2.2046).toFixed(2));
+  var mediaPeso = (cavaleiros || goldSaints).filter(apenasComPeso)
+                            .map(function(e){
+                              cavComPeso++;
+                              return e.pesoLb;
+                            })
+                            .reduce(function(acc, e){
+                              return acc + e;
+                            }, 0) / cavComPeso;
+  return arredonda(lbParaKg(mediaPeso));
 }
 
 function obterPesoMedioDoadores() {
   var doadores = obterDoadores();
-  var pesoTotDoadores = 0;
-  var cavComPeso = 0;
-  for (var i in doadores) {
-    if ('pesoLb' in doadores[i]) {
-      cavComPeso++;
-      pesoTotDoadores = pesoTotDoadores + doadores[i].pesoLb;
-    }
-  }
-  return parseFloat(((pesoTotDoadores/cavComPeso)/2.2046).toFixed(2));
+  return obterPesoMedio(doadores);
 }
 
 function obterIMC() {
-  var indices = [];
-  for (var i in goldSaints) {
-    if ('pesoLb' in goldSaints[i]) {
-      var pesoKg = parseFloat(((goldSaints[i].pesoLb)/2.2046).toFixed(2));
-      var altura = (goldSaints[i].alturaCm/100);
-      var imc = parseFloat((pesoKg/(altura*altura)).toFixed(2));
-      indices.push(imc);
-    }
-  }
-  return indices;
+  return goldSaints.filter(apenasComPeso)
+                   .map(function(e) {
+                     var imc = lbParaKg(e.pesoLb) / Math.pow(cmParaMetros(e.alturaCm), 2);
+                     return arredonda(imc);
+                   });
 }
 
 function obterSobrepeso() {
-  var sobrepesos = [];
-  for (var i in goldSaints) {
-    if ('pesoLb' in goldSaints[i]) {
-      var pesoKg = parseFloat(((goldSaints[i].pesoLb)/2.2046).toFixed(2));
-      var altura = (goldSaints[i].alturaCm/100);
-      var imc = parseFloat((pesoKg/(altura*altura)).toFixed(2));
-      if(imc >= 25) {
-        sobrepesos.push(goldSaints[i]);
-      }
-    }
-  }
-  return sobrepesos;
+  var imcs = obterIMC();
+  return goldSaints.filter(function(e, indice) {
+    var imc = imcs[indice];
+    return 25 <= imc && imc < 30;
+  });
 }
