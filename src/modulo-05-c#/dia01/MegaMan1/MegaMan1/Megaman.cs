8bfcea6f3a075366da4b4eea279e8e2cef750040
Marcelo Warth de Moura <marcelo.moura@cwinet.local>
2016-05-06 17:29:19 -0300

    feat(c# dia01): implementacoes dia 01 de c#

---
+++ src/modulo-05-c#/dia01/MegaMan1/MegaMan1/Megaman.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaMan1
+{
+    public class Megaman : Robo
+    {
+
+    }
+}
---
+++ src/modulo-05-c#/dia01/MegaMan1/MegaMan1/Robo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaMan1
+{
+    public abstract class Robo
+    {
+        public abstract string Nome { get; }
+        private int vida;
+        public virtual int Vida()
+        {
+            get
+            {
+                return vida;
+            }
+            private set
+            {
+
+            }
+        }
+        public int Ataque
+        {
+            get
+            {
+                return 5;
+            }         
+        }
+        public virtual int Defesa
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        public virtual void Atacar(Robo robo) { }
+        public virtual void ReceberAtaque(int dano) { }
+    }
+}
