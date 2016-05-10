f307bed189fd70f5e1a896e4780e333d6c710a76
Marcelo Moura <Marcelo Moura>
2016-05-10 08:27:49 -0300

    feat(dia02): exemplos e resolucao dos exercicios

---
+++ src/modulo-05-c#/dia02/ExercicioLambdaLinq/RepositorioFuncionarios/RepositorioFuncionarios.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Repositorio
+{
+    public class RepositorioFuncionarios
+    {
+        public List<Funcionario> Funcionarios { get; private set; }
+
+        public RepositorioFuncionarios()
+        {
+            CriarBase();
+        }
+
+        private void CriarBase()
+        {
+            Funcionarios = new List<Funcionario>();
+
+            Cargo desenvolvedor1 = new Cargo("Desenvolvedor Júnior", 190);
+            Cargo desenvolvedor2 = new Cargo("Desenvolvedor Pleno", 250);
+            Cargo desenvolvedor3 = new Cargo("Desenvolvedor Sênior", 550.5);
+
+            Funcionario lucasLeal = new Funcionario(1, "Marcelinho Carioca", new DateTime(1995, 01, 24));
+            lucasLeal.Cargo = desenvolvedor1;
+            lucasLeal.TurnoTrabalho = TurnoTrabalho.Manha;
+            Funcionarios.Add(lucasLeal);
+
+            Funcionario jeanPinzon = new Funcionario(2, "Mark Zuckerberg", new DateTime(1991, 04, 25));
+            jeanPinzon.Cargo = desenvolvedor1;
+            jeanPinzon.TurnoTrabalho = TurnoTrabalho.Tarde;
+            Funcionarios.Add(jeanPinzon);
+
+            Funcionario rafaelBenetti = new Funcionario(3, "Aioros de Sagitário", new DateTime(1991, 08, 15));
+            rafaelBenetti.Cargo = desenvolvedor1;
+            rafaelBenetti.TurnoTrabalho = TurnoTrabalho.Noite;
+            Funcionarios.Add(rafaelBenetti);
+
+            Funcionario mauricioBorges = new Funcionario(4, "Uchiha Madara", new DateTime(1996, 11, 30));
+            mauricioBorges.Cargo = desenvolvedor1;
+            mauricioBorges.TurnoTrabalho = TurnoTrabalho.Manha;
+            Funcionarios.Add(mauricioBorges);
+
+            Funcionario leandroAndreolli = new Funcionario(5, "Barack Obama", new DateTime(1990, 03, 07));
+            leandroAndreolli.Cargo = desenvolvedor1;
+            leandroAndreolli.TurnoTrabalho = TurnoTrabalho.Manha;
+            Funcionarios.Add(leandroAndreolli);
+
+            Funcionario felipeNervo = new Funcionario(6, "Whindersson  Nunes", new DateTime(1994, 01, 12));
+            felipeNervo.Cargo = desenvolvedor1;
+            felipeNervo.TurnoTrabalho = TurnoTrabalho.Tarde;
+            Funcionarios.Add(felipeNervo);
+
+            Funcionario lucasKauer = new Funcionario(7, "Optimus Prime", new DateTime(1997, 05, 10));
+            lucasKauer.Cargo = desenvolvedor1;
+            lucasKauer.TurnoTrabalho = TurnoTrabalho.Noite;
+            Funcionarios.Add(lucasKauer);
+
+            Funcionario eduardoArnold = new Funcionario(8, "Arnold Schwarzenegger", new DateTime(1989, 09, 16));
+            eduardoArnold.Cargo = desenvolvedor1;
+            eduardoArnold.TurnoTrabalho = TurnoTrabalho.Tarde;
+            Funcionarios.Add(eduardoArnold);
+
+            Funcionario gabrielAlboy = new Funcionario(9, "Bill Gates", new DateTime(1990, 02, 25));
+            gabrielAlboy.Cargo = desenvolvedor2;
+            gabrielAlboy.TurnoTrabalho = TurnoTrabalho.Manha;
+            Funcionarios.Add(gabrielAlboy);
+
+            Funcionario carlosHenrique = new Funcionario(10, "Linus Torvalds", new DateTime(1965, 12, 02));
+            carlosHenrique.Cargo = desenvolvedor2;
+            carlosHenrique.TurnoTrabalho = TurnoTrabalho.Tarde;
+            Funcionarios.Add(carlosHenrique);
+
+            Funcionario margareteRicardo = new Funcionario(11, "Dollynho Developer", new DateTime(1980, 10, 10));
+            margareteRicardo.Cargo = desenvolvedor3;
+            margareteRicardo.TurnoTrabalho = TurnoTrabalho.Manha;
+            Funcionarios.Add(margareteRicardo);
+        }
+
+        public IList<Funcionario> BuscarPorCargo(Cargo cargo)
+        {
+            return Funcionarios.Where(x => x.Cargo.Equals(cargo)).ToList();
+        }
+
+        public IList<Funcionario> OrdenadosPorCargo()
+        {
+            return Funcionarios.OrderBy(x => x.Cargo.Titulo).ThenBy(x => x.Nome).ToList();
+        }
+
+        public IList<Funcionario> BuscarPorNome(string nome)
+        {
+            return Funcionarios.Where(x => x.Nome.ToLowerInvariant()
+                                            .Contains(nome.ToLowerInvariant())).ToList();
+        }        
+
+        public IList<Funcionario> BuscarPorTurno(params TurnoTrabalho[] turnos)
+        {
+            return Funcionarios.Where(x => turnos.Contains(x.TurnoTrabalho)).ToList();
+        }        
+
+        public IList<Funcionario> FiltrarPorIdadeAproximada(int idade)
+        {
+            int ano = DateTime.Today.Year;
+            return Funcionarios.Where(x => ano - x.DataNascimento.Year <= idade + 5 
+                                        && ano - x.DataNascimento.Year >= idade - 5).ToList();
+        }        
+
+        public double SalarioMedio(TurnoTrabalho? turno = null)
+        {
+            var funcs = turno == null ? Funcionarios : Funcionarios.Where(x => x.TurnoTrabalho == turno);
+            return funcs.Average(x => x.Cargo.Salario);
+        }
+
+        public IList<Funcionario> AniversariantesDoMes()
+        {
+            int mes = DateTime.Today.Month;
+            return Funcionarios.Where(x => x.DataNascimento.Month == mes).ToList();
+        }
+
+        public IList<dynamic> BuscaRapida()
+        {
+            return Funcionarios.Select(x => new {
+                NomeFuncionario = x.Nome,
+                TituloCargo = x.Cargo.Titulo
+            }).ToArray();
+        }
+
+        public IList<dynamic> QuantidadeFuncionariosPorTurno()
+        {
+            return Funcionarios.GroupBy(x => x.TurnoTrabalho)
+                            .Select(x => new {
+                                Turno = x.Key,
+                                Quantidade = x.Count()}).ToArray();
+        }
+
+        public dynamic FuncionarioMaisComplexo()
+        {
+            return Funcionarios.Select(x => new
+            {
+                Nome = x.Nome,
+                DataNascimento = x.DataNascimento.ToString("dd/MM/yyyy"),
+                SalarioRS = string.Format("R$ {0}", x.Cargo.Salario.ToString("F")),
+                SalarioUS = string.Format("${0}", x.Cargo.Salario.ToString("F").Replace(",", ".")),
+                QuantidadeMesmoCargo = Funcionarios.Where(y => y.Cargo.Equals(x.Cargo)).Count()
+            }).Where(x => x.Nome.Equals("Dollynho Developer")).ToArray()[0];
+        }
+    }
+}
