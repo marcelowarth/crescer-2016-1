57274e535e5478f87893eba3e1f55adbcf1822c1
Marcelo Moura <marcelowarth@gmail.com>
2016-05-13 09:33:09 -0300

    feat(dia05): ajuste do projeto do fabricio para o meu

---
+++ src/modulo-05-c#/dia05/LojaNinja/LojaNinja.MVC/Models/PedidoModel.cs
@@ -0,0 +1,46 @@
+﻿using LojaNinja.Dominio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LojaNinja.MVC.Models
+{
+    public class PedidoModel
+    {
+        public int? Id { get; set; }
+
+        [Required(ErrorMessage = "Data de Entrega deve ser informada")]
+        [DisplayName("Data de Entrega")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime DataEntrega { get; set; }
+
+        [Required(ErrorMessage = "Nome do Produto deve ser informado")]
+        [DisplayName("Nome do Produto")]
+        public string NomeProduto { get; set; }
+
+        [Required(ErrorMessage = "Valor deve ser informado")]
+        [DisplayName("Valor")]
+        [DisplayFormat(DataFormatString = "{0:c}", ApplyFormatInEditMode = true)]
+        public Decimal Valor { get; set; }
+
+        [Required]
+        public TipoPagamento TipoPagamento { get; set; }
+
+        [Required]
+        [DisplayName("Tipo de Pagamento")]
+        public TipoPagamento TipoDePagamento { get; set; }
+
+        [Required(ErrorMessage = "Nome do Cliente deve ser informado")]
+        [DisplayName("Nome do Cliente")]
+        public string NomeCliente { get; set; }
+
+        [Required(ErrorMessage = "Cidade deve ser informada")]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "Estado deve ser informado")]
+        public string Estado { get; set; }
+    }
+}
