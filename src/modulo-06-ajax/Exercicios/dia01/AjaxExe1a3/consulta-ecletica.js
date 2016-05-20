aed7ed46116d1cf39e7605ea783c0a2c8204a4a6
Marcelo Moura <marcelowarth@gmail.com>
2016-05-20 10:23:47 -0300

    fix(consulta-ecletica): correcao do exercicio 2 que estava com problema feat(consulta-ecletica): resolucao do exercicio 3

--- src/modulo-06-ajax/Exercicios/dia01/AjaxExe1a3/consulta-ecletica.js
+++ src/modulo-06-ajax/Exercicios/dia01/AjaxExe1a3/consulta-ecletica.js
@@ -1,42 +1,35 @@
 'use strict';
 
-var resultadoBusca;
-var resBuscaURI;
-var urlBusca;
-
-$('#btnBuscar').click(
-  function() {
-    resultadoBusca = $('#txtBusca').val();
-    encodaURI();
-    buscaArtistaId();
-    carregarDadosNaPagina();
-  }
-);
-
-function encodaURI() {
-  resBuscaURI = encodeURI(resultadoBusca);
-};
-
-function buscaArtistaId() {
-  var urlAppend = 'https://api.spotify.com/v1/artists/' + resBuscaURI +'/albums?limit=50'
-  $.ajax({
-    url: urlAppend, type: 'GET' })
-    .done(function(res){
-      urlBusca = 'https://api.spotify.com/v1/artists/' + res.artists.items[0].id + '/albums?limit=50'
-    });
-}
-
-function carregarDadosNaPagina() {
-  $.ajax({ url: urlBusca, type: 'GET' })
-  .done(function (res) {
-    console.log(res);
-    res.items.forEach(function(item){
-      item.images.forEach(function(img){
-        var imageUrl = img.url;
-        $('#divImagens').append(
-          $('<img width="300" height="300">').attr('src', imageUrl)
-        );
-      });
-    });
+$(function() {
+  $('#formBusca').submit(function(e) {
+    var resultadoBusca = ($('#txtBusca').val());
+    var artistaDados = buscaArtistaDados(resultadoBusca).done(
+      function(res) {
+          var pesquisa = (resultadoBusca === 'justin bieber' && Math.random() < 0.8) ? 'douchebag' : res.artists.items[0].id;
+          var artistaCompleto = buscaArtistaCompleto(pesquisa)
+            .then(
+              function(data) {
+                var capa = data.items;
+                mostraCapa(capa);
+              }, function (data) {
+                alert('Caro usuário, devido a um erro '+ data.status +', não foi possível pesquisar por ' + pesquisa);
+              });
+        });
+    return e.preventDefault();
   });
+});
+
+function buscaArtistaDados(resultadoBusca) {
+  return $.get('https://api.spotify.com/v1/search?q=' + encodeURI(resultadoBusca) + '&type=artist')
 };
+
+function buscaArtistaCompleto(id) {
+  return $.get('https://api.spotify.com/v1/artists/' + id + '/albums?limit=50');
+};
+
+function mostraCapa (capa) {
+  capa.forEach(function(e) {
+    $('#divImagens').append(
+      $('<img>').attr('src', e.images[1].url))
+  });
+}
