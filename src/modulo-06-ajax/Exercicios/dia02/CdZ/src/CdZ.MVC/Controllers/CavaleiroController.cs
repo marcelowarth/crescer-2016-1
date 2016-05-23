a724da91f450d76a2ae8f062164bbef6880a6aa8
Marcelo Moura <marcelowarth@gmail.com>
2016-05-22 23:11:20 -0300

    feat(CdZ): atualizaçao para versao mais recente do codigo do bernardo e realizacao do exercicio 1 com extra point

--- src/modulo-06-ajax/Exercicios/dia02/CdZ/src/CdZ.MVC/Controllers/CavaleiroController.cs
+++ src/modulo-06-ajax/Exercicios/dia02/CdZ/src/CdZ.MVC/Controllers/CavaleiroController.cs
@@ -1,4 +1,6 @@
 ﻿using CdZ.Dominio;
+using CdZ.MVC.Extensions;
+using CdZ.MVC.Filters;
 using CdZ.MVC.Models.Cavaleiro;
 using CdZ.MVC.Services;
 using System.Net;
@@ -6,6 +8,7 @@ using System.Web.Mvc;
 
 namespace CdZ.MVC.Controllers
 {
+    [CdZAutorizador]
     public class CavaleiroController : Controller
     {
         private ICavaleiroRepositorio _cavaleiros = ServicoInjecaoDeDependecia.CriarCavaleiroRepositorio();
@@ -24,7 +27,14 @@ namespace CdZ.MVC.Controllers
                 throw new HttpException(status, "Ops");
             */
             //System.Threading.Thread.Sleep(3000);
-            return Json(new { data = _cavaleiros.Todos() }, JsonRequestBehavior.AllowGet);
+            return Json(new { data = _cavaleiros.Todos().FromModel() }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        [ActionName("GetById")]
+        public JsonResult Get(int? id)
+        {
+            return Json(new { data = _cavaleiros.Buscar(id.Value).FromModel() }, JsonRequestBehavior.AllowGet);
         }
 
         [HttpPost]
@@ -34,6 +44,28 @@ namespace CdZ.MVC.Controllers
             var novoId = _cavaleiros.Adicionar(cavaleiro.ToModel());
             Response.StatusCode = (int)HttpStatusCode.Created;
             return Json(new { id = novoId });
+        }
+
+        [HttpDelete]
+        public JsonResult Delete(int id)
+        {
+            _cavaleiros.Excluir(id);
+            return NoContentJsonVazio();
+            // OU retornar a lista atualizada de todos cavaleiros
+            // return Json(new { data = _cavaleiros.Todos() });
+        }
+
+        [HttpPut]
+        public JsonResult Put(CavaleiroViewModel cavaleiro)
+        {
+            _cavaleiros.Atualizar(cavaleiro.ToModel());
+            return NoContentJsonVazio();
+        }
+
+        private JsonResult NoContentJsonVazio()
+        {
+            Response.StatusCode = (int)HttpStatusCode.NoContent;
+            return Json(new { });
         }
     }
 }
