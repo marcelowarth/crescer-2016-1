c034c722b14a75cf4cce31de5fc827e0324065ac
Marcelo Moura <marcelowarth@gmail.com>
2016-06-02 14:19:53 -0300

    feat(dia02-parte2): resolucao da parte 2

---
+++ src/modulo-07-banco-parte2/exercicios/dia02/CRESCER16-Parte2.sql
@@ -0,0 +1,43 @@
+--1) Crie um procedimento que receba por parâmetro o IDPedido e atualize o valor do pedido conforme seus itens.
+--? Valor unitário * Quantidade
+CREATE OR REPLACE PROCEDURE ATUALIZA_VALOR_PEDIDO (pIDPedido in integer) AS
+BEGIN
+  UPDATE PEDIDO ped 
+  SET ped.VALORPEDIDO = (SELECT SUM(pItem.QUANTIDADE*pItem.PRECOUNITARIO) 
+                          FROM PEDIDOITEM pItem 
+                          WHERE pItem.IDPedido = ped.IDPedido)
+  WHERE ped.IDPEDIDO = pIDPedido;
+END;
+
+--2) Crie uma função que receba por parâmetro o IDCliente e retorne a data do último pedido realizado pelo cliente.
+CREATE OR REPLACE FUNCTION ULTIMO_PEDIDO (pIDCliente in integer) RETURN PEDIDO.DATAPEDIDO%type AS
+  vUltPed PEDIDO.DATAPEDIDO%type;
+BEGIN
+  SELECT MAX(DATAPEDIDO)
+  INTO vUltPed
+  FROM PEDIDO
+  WHERE IDCLIENTE = pIDCliente;
+  
+  RETURN vData;
+END;
+
+--3) Crie uma função que receba por parâmetro o IDProduto e o período (mês e ano) e retorne a quantidade (total)
+--desde produtos vendidos no período (considere todos os dias do mês).
+CREATE OR REPLACE FUNCTION PRODUTOS_PERIODO (pIDProduto in integer, pData in DATE) RETURN PEDIDOITEM.QUANTIDADE%type AS
+  vQtde PEDIDOITEM.QUANTIDADE%type;
+BEGIN
+  SELECT SUM(pItem.QUANTIDADE)
+  INTO vQtde
+  FROM PEDIDOITEM pItem
+  JOIN PEDIDO ped on pItem.IDPEDIDO = ped.IDPEDIDO
+  WHERE ped.DATAPEDIDO BETWEEN EXTRACT(month FROM pData) AND LAST_DAY(pData)
+    AND pItem.IDPRODUTO = pIDProduto;
+    
+  RETURN vQtde;
+END;
+
+
+
+
+
+
