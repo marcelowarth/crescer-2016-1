80f22fe562557ebfac2a78f8d17f70fa64b0e116
Marcelo Moura <marcelowarth@gmail.com>
2016-06-15 17:22:02 -0300

    feat(aula6): parte do conteudo visto em aula

---
+++ src/modulo-08-java/exercicios/Aula6/crescer-web/src/main/java/br/cwi/crescer/Hello.java
@@ -0,0 +1,20 @@
+package br.cwi.crescer;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class Hello implements Serializable {
+
+    private String helloWorld = "Hello World!";
+
+    public String getHelloWorld() {
+        return helloWorld;
+    }
+
+    public void setHelloWorld(String helloWorld) {
+        this.helloWorld = helloWorld;
+    }
+
+}
